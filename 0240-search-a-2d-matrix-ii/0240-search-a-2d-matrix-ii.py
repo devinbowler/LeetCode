4caebf41639eaee4d33fd13e06731baf4999d98d
Devin Bowler <83326036+devinbowler@users.noreply.github.com>
2025-06-16 19:33:41 -0400

    0240-SEARCH-A-2D-MATRIX-II Stats: Time: 177 ms (5.67%), Space: 24.1 MB (22.15%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -3,10 +3,27 @@ class Solution:
         # One solution could be a binary search on each row, but that seems
         # still a bit much.
         # Lets brute force this first.
+        # What if we check the diagonal for greater or less than, if great we can eleminate
+        # that row and column behind it but if less than we check the row and col behind it.
+        m, n = len(matrix), len(matrix[0])
+        def checkCorner(x, y):
+            # Check x
+            for idx in range(n):
+                print(matrix[y][idx])
+                if matrix[y][idx] == target:
+                    return True
+            # Check y
+            for idx in range(m):
+                if matrix[idx][x] == target:
+                    return True
+            return False
 
-        for row in range(len(matrix)):
-            for col in range(len(matrix[0])):
-                if matrix[row][col] == target:
+        for coord in range(min(m, n)):
+            curNum = matrix[coord][coord]
+            if curNum == target:
+                return True
+            else:
+                if checkCorner(coord, coord):
                     return True
 
         return False
